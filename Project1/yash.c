2928f8a5c1ba71856b0cd51acaae7e209255a9b7
sid <siddharth.benoy@utexas.edu>
2024-09-16 11:57:02 -0500

    updated for clion

--- Project1/yash.c
+++ Project1/yash.c
@@ -16,10 +16,12 @@
 #include <readline/history.h>
 
 #define MAXINPUTS 100
+#define MAXLABELS 10
 
-void execute_command(char * command, char * arguments[], int command_count);
-void file_redirection_piping(char * command, char * arguments[], int token_count);
+void execute_command(char * command, char * arguments[], int token_count);
+void file_redirection_piping(char * command, char * command2, char * arguments[], int token_count);
 void handle_signal(int signumber);
+void clear_args(char * arguments[]);
 
 int main()
 {
@@ -31,7 +33,7 @@ int main()
 
     command_index = token_count = 0;
 
-    while (line = readline("# "))
+    while ((line = readline("# ")))
     {
         token = strtok(line," ");
         commands[command_index] = token;
@@ -46,16 +48,15 @@ int main()
 
         execute_command(commands[0], commands, token_count);
 
-        for (int i = 0; i < MAXINPUTS; i++)
-        {
-            commands[i] = '\0';
-        }
+        clear_args(commands);
         
-        if (signal(SIGHUP, handle_signal) == SIG_ERR)
-        {
-            printf("caught signal");
-        }
+//        if (signal(SIGHUP, handle_signal) == SIG_ERR)
+//        {
+//            printf("caught signal");
+//        }
         command_index = 0;
+        token_count = 0;
+        free(line);
     }
 
 }
@@ -64,13 +65,19 @@ int main()
 
 void execute_command(char * command, char * arguments[], int token_count)
 {
-    int cpid, error, redirection_flag, file_descriptor;
+    int cpid, error, redirection_flag, file_descriptor, sub_args_index;
+
+    sub_args_index = 0;
+
+    char * sub_args[MAXINPUTS];
 
     for (int i = 0; i < token_count; i++)
     {
         if (strcmp(arguments[i], "|") == 0)
         {
-            file_redirection_piping(command, arguments, token_count);
+            char * command2 = arguments[i+1];
+            file_redirection_piping(command, command2, arguments, token_count);
+            //check for bg/fg
             return;
         }
         else if (strcmp(arguments[i], "<") == 0 && i+1 == token_count-1)
@@ -79,13 +86,14 @@ void execute_command(char * command, char * arguments[], int token_count)
             cpid = fork();
             if (cpid == 0)
             {
-                dup2(file_descriptor, STDOUT_FILENO);
-                execlp(command, command, NULL);
+                dup2(file_descriptor, STDIN_FILENO);
+                execvp(command, sub_args);
             }
             else
             {
                 wait(NULL);
             }
+            close(file_descriptor);
             return;
         }
         else if (strcmp(arguments[i], ">") == 0 && i+1 == token_count-1)
@@ -94,13 +102,15 @@ void execute_command(char * command, char * arguments[], int token_count)
             cpid = fork();
             if (cpid == 0)
             {
-                dup2(file_descriptor, STDIN_FILENO);
-                execlp(command, command, NULL);
+                dup2(file_descriptor, STDOUT_FILENO);
+                execvp(command, sub_args);
             }
             else
             {
                 wait(NULL);
             }
+            close(file_descriptor);
+            clear_args(sub_args);
             return;
         }
         else if (strcmp(arguments[i], "2>") == 0 && i+1 == token_count-1)
@@ -110,13 +120,20 @@ void execute_command(char * command, char * arguments[], int token_count)
             if (cpid == 0)
             {
                 dup2(file_descriptor, STDERR_FILENO);
-                execlp(command, command, NULL);
+                execvp(command, sub_args);
             }
             else
             {
                 wait(NULL);
             }
+            close(file_descriptor);
             return;
+        }
+        else
+        {
+            sub_args[sub_args_index] = arguments[i];
+            sub_args[sub_args_index + 1] = NULL;
+            sub_args_index++;
         }
     }
 
@@ -136,25 +153,73 @@ void execute_command(char * command, char * arguments[], int token_count)
 
 }
 
-void file_redirection_piping(char * command, char * arguments[], int token_count)
+void file_redirection_piping(char * command, char * command2, char * arguments[], int token_count)
 {
+    int pdf[2];
+    int cpid, lpid, argument_index;
+    argument_index = 0;
+    char * left_arguments[MAXINPUTS];
+    char * right_arguments[MAXINPUTS];
 
-}
-
-void handle_signal(int signumber)
-{
-    switch(signumber)
+    for (int i = 0; i < token_count; i++)           //gets command options into two arrays for both commands
     {
-        case SIGHUP:
-            printf("caught sighup aka the ending\n");
-            exit(0);
-            break;
-        case SIGINT:
-            printf("caught sigint\n");
-            exit(0);
-            break;
-        case SIGTSTP:
-            printf("caught sigtstp\n");
-            break;
+        while ((strcmp(arguments[i], ">") == 0) || (strcmp(arguments[i], "<") == 0) || (strcmp(arguments[i], "2>") ==0))
+        {
+            left_arguments[argument_index] = arguments[i];
+            argument_index++;
+            left_arguments[argument_index] = NULL;
+            i++;
+        }
+        argument_index = 0;
+        if (strcmp(arguments[i], command2) == 0)
+        {
+            while ((strcmp(arguments[i], ">") == 0) || (strcmp(arguments[i], "<") == 0) || (strcmp(arguments[i], "2>") ==0))
+            {
+                right_arguments[argument_index] = arguments[i];
+                argument_index++;
+                right_arguments[argument_index] = NULL;
+                i++;
+            }
+        }
+    }
+
+
+
+    if (pipe(pdf) == -1)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < token_count; i++)
+    {
+
     }
 }
+
+void clear_args(char * arguments[])
+{
+    for (int i = 0; i < MAXINPUTS; i++)
+    {
+        arguments[i] = '\0';
+    }
+}
+
+//void handle_signal(int signumber)
+//{
+//    switch(signumber)
+//    {
+//        case SIGHUP:
+//            printf("caught sighup aka the ending\n");
+//            exit(0);
+//        case SIGINT:
+//            printf("caught sigint\n");
+//            exit(0);
+//        case SIGTSTP:
+//            printf("caught sigtstp\n");
+//            break;
+//        default:
+//            printf("hi");
+//            break;
+//    }
+//}
+
