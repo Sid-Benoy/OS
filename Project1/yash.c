14b31d9694b97c2a66be0b0a1463344c9349b3b6
sid <siddharth.benoy@utexas.edu>
2024-09-17 18:59:19 -0500

    seeing what i can do

--- Project1/yash.c
+++ Project1/yash.c
@@ -16,19 +16,21 @@
 #include <readline/history.h>
 
 #define MAXINPUTS 100
+#define MAXCHARS 2000
 #define MAXLABELS 10
 
 typedef enum {Stopped, Running, Done} status;
 
 mode_t mode = S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH;
 int jid = 0;
+int child_id = 0;
 
 void execute_command(char * command, char * arguments[], int token_count);
 void file_redirection_piping(char * command, char * command2, char * arguments[], int token_count);
 void sig_handler(int signumber);
 void clear_args(char * arguments[]);
 void printjobs();
-
+void add_job(pid_t pid, char * command, status stat, bool bg);
 struct Job
 {
     int job_id;
@@ -39,11 +41,19 @@ struct Job
     struct job * next;
 };
 
+struct Job * jobs[MAXINPUTS];
+int job_index = 0;
+
+void add_job(pid_t pid, char * command, status t, bool bg)
+{
+
+}
 int main()
 {
+
+    signal(SIGCHLD, sig_handler);
     signal(SIGINT, sig_handler);
     signal(SIGTSTP, sig_handler);
-    signal(SIGCHLD, sig_handler);
 
     pid_t pid;
     char * commands[MAXINPUTS];
@@ -55,6 +65,8 @@ int main()
 
     while ((line = readline("# ")))
     {
+        if(strcmp(line, "") == 0)
+            continue;
 
         add_history(line);
         token = strtok(line," ");
@@ -85,11 +97,6 @@ int main()
         execute_command(commands[0], commands, token_count);
 
         clear_args(commands);
-        
-//        if (signal(SIGHUP, handle_signal) == SIG_ERR)
-//        {
-//            printf("caught signal");
-//        }
         command_index = 0;
         token_count = 0;
         free(line);
@@ -101,11 +108,16 @@ int main()
 
 void execute_command(char * command, char * arguments[], int token_count)
 {
-    int cpid, error, redirection_flag, file_descriptor_in, file_descriptor_out, file_descriptor_err, sub_args_index;
+    int cpid, file_descriptor_in, file_descriptor_out, file_descriptor_err, sub_args_index;
+
+    file_descriptor_in = -1;
+    file_descriptor_out = -1;
+    file_descriptor_err = -1;
+
     bool redirection = false;
     struct Job new_job;
 
-    bool is_bg;
+    bool is_bg = false;
     sub_args_index = 1;
 
     char * sub_args[MAXINPUTS];
@@ -133,7 +145,6 @@ void execute_command(char * command, char * arguments[], int token_count)
             file_descriptor_in = open(arguments[i+1], O_RDONLY, mode);
             redirection = true;
             i++;
-
         }
         else if (strcmp(arguments[i], ">") == 0 && arguments[i+1] != NULL)
         {
@@ -178,42 +189,54 @@ void execute_command(char * command, char * arguments[], int token_count)
                 close(file_descriptor_err);
             }
             setpgid(0, 0);
+
             execvp(command, sub_args);
         }
         else
         {
-            wait(NULL);
+            child_id = cpid;
+            setpgid(cpid, cpid);
+            //wait(NULL);
+            pause();
         }
+
         clear_args(sub_args);
         return;
     }
-    new_job.bg = is_bg;
-    new_job.job_id = jid;
-    new_job.jstr = strcat(command, (const char *) arguments);
 
-    printf("%s", new_job.jstr);
+    char new_jstr[MAXCHARS];
+    for (int i = 0; arguments[i] != NULL; i++)
+    {
+        strcat(new_jstr, arguments[i]);
+    }
+
+    int status, x;
 
     cpid = fork();
     if (cpid == 0)
     {
-
-        setpgid(0, 0);
-        new_job.pgid = getppid();
-        new_job.state = Running;
+        setpgid(0,0);
         execvp(command, arguments);
     }
     else if (is_bg)
     {
-        new_job.state = Running;
+        add_job(cpid, new_jstr, Running, is_bg);
     }
     else
     {
-        wait((int*) NULL);
+        setpgid(cpid, cpid);
+        child_id = cpid;
+        //pause();
+        x = waitpid(cpid, &status, WUNTRACED | WNOHANG );
+
+        printf("%d\n", x);
+        printf("%d\n", status);
+
+        new_job.state = Done;
     }
+
     clear_args(sub_args);
     is_bg = false;
-
-
 }
 
 void file_redirection_piping(char * command, char * command2, char * arguments[], int token_count) {
@@ -372,6 +395,8 @@ void file_redirection_piping(char * command, char * command2, char * arguments[]
     close(pdf[1]);
     wait((int *)NULL);
     wait((int *)NULL);
+    clear_args(left_arguments);
+    clear_args(right_arguments);
 
 }
 
@@ -385,30 +410,47 @@ void clear_args(char * arguments[])
 
 void printjobs()
 {
-
+    if (job_index == 0)
+        return;
+    for (int i = 0; jobs[i]->jstr != NULL; i++)
+    {
+        if (strcmp(jobs[i]->jstr, "Running") == 0)
+        {
+            printf("[%d] Running", jobs[i]->job_id);
+        }
+        else if (strcmp(jobs[i]->jstr, "Started") == 0)
+        {
+            printf("[%d] Started", jobs[i]->job_id);
+        }
+        else if (strcmp(jobs[i]->jstr, "Done") == 0)
+        {
+            printf("[%d] Done", jobs[i]->job_id);
+            jobs[i] = NULL;
+        }
+    }
 }
 
 void sig_handler(int signumber)
 {
-    pid_t pid;
+    pid_t pid = tcgetpgrp(STDIN_FILENO);
+    int status;
     switch(signumber)
     {
         case SIGINT:
-            pid = tcgetpgrp(STDIN_FILENO);
-            if (kill(-pid, SIGINT) == 0)
+            if (killpg(child_id, SIGINT) == 0)
             {
-                printf("hi");
+
             }
-            else
-                perror("error");
+            child_id = 0;
             break;
         case SIGTSTP:
-            tcsetpgrp(STDIN_FILENO, getpid());
-            printf("\n");
-            printf("# ");
+            if (killpg(child_id, SIGTSTP) == 0)
+            {
+
+            }
             break;
         case SIGCHLD:
-            while (waitpid(-1, NULL, WNOHANG) > 0);
+            while (waitpid(-1, NULL, WNOHANG | WUNTRACED) > 0);
             break;
 
     }
